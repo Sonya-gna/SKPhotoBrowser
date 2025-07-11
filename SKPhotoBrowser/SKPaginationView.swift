00aa9e793db99672b8427648fee2ef739c47815a
Sonya <sonya.hong@gna.company>
2025-07-11 14:29:22 +0900

    Change pagination y position

--- SKPhotoBrowser/SKPaginationView.swift
+++ SKPhotoBrowser/SKPaginationView.swift
@@ -12,7 +12,7 @@ class SKPaginationView: UIView {
     var counterLabel: UILabel?
     var prevButton: UIButton?
     var nextButton: UIButton?
-    private var margin: CGFloat = 100
+    private var margin: CGFloat = 40
     private var extraMargin: CGFloat = SKMesurement.isPhoneX ? 40 : 0
     
     fileprivate weak var browser: SKPhotoBrowser?
@@ -27,7 +27,8 @@ class SKPaginationView: UIView {
     
     convenience init(frame: CGRect, browser: SKPhotoBrowser?) {
         self.init(frame: frame)
-        self.frame = CGRect(x: 0, y: frame.height - margin - extraMargin, width: frame.width, height: 100)
+//        self.frame = CGRect(x: 0, y: frame.height - margin - extraMargin, width: frame.width, height: 100)
+        self.frame = CGRect(x: 0, y: margin, width: frame.width, height: 100)
         self.browser = browser
 
         setupApperance()
@@ -53,7 +54,8 @@ class SKPaginationView: UIView {
     }
     
     func updateFrame(frame: CGRect) {
-        self.frame = CGRect(x: 0, y: frame.height - margin, width: frame.width, height: 100)
+//        self.frame = CGRect(x: 0, y: frame.height - margin, width: frame.width, height: 100)
+        self.frame = CGRect(x: 0, y: margin, width: frame.width, height: 100)
     }
     
     func update(_ currentPageIndex: Int) {
